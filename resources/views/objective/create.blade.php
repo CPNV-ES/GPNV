bbdd4a1f617e5f9e0db464f6f73958d20b69c836
Antonio Giordano <antonio.giordano17@gmail.com>
2018-04-20 14:11:22 +0200

    Update blades for checklist creation

--- resources/views/objective/create.blade.php
+++ resources/views/objective/create.blade.php
@@ -11,7 +11,7 @@ Requirement: Link checkList.js
 
                 <input type="text" name="name" id="name" required>
                 <input type="hidden" name="description" value="">
-                <button type="button" id="newCheckList" class="glyphicon glyphicon-floppy-save" aria-hidden="true"></button>
+                <button type="button" class="newCheckList glyphicon glyphicon-floppy-save" aria-hidden="true"></button>
                 <a class="btn cancelNewObjective pull-right"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
             </form>
         </div>
