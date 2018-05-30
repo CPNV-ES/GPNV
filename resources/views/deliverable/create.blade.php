2660050d444ff799d2c4d5ec04c69e365d266df3
Antonio Giordano <antonio.giordano17@gmail.com>
2018-05-30 12:09:06 +0200

    Update blades for deliverables

--- resources/views/deliverable/create.blade.php
+++ resources/views/deliverable/create.blade.php
@@ -3,18 +3,18 @@ Created By: Antonio Giordano
 Description: Form to add a new checkList
 Requirement: Link checkList.js
 -->
-<div class="well well-sm formNewDelivery hidden" style="max-height: 35px;" >
-    <div class="media">
-        <div class="media-body" style="position: relative;top: -8px;">
-            <form class="form-horizontal" role="form" id="form" action="{{ URL('project') }}/{{$project->id}}/checklist/{{$livrables->getId()}}/create">
-                {{ csrf_field() }}
-
-                <input type="text" name="name" id="name" required>
-                <input type="hidden" name="description" value="">
-                <button type="button" class="newCheckList glyphicon glyphicon-floppy-save" aria-hidden="true"></button>
-                <a class="btn cancelNewDelivery pull-right"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
-            </form>
+<div class="form-new-deliverable" style="max-height: 35px;">
+    <form class="form-horizontal" role="form" id="form" action="{{ URL('project') }}/{{$project->id}}/checklist/{{$livrables->getId()}}/create">
+        <div class="col-md-6">
+            {{ csrf_field() }}
+            <input type="text" name="name" id="name" class="form-control" required>
+            <input type="hidden" name="description" value="">
         </div>
-    </div>
+        <div class="col-md-1">
+            <button type="button" class="newCheckList btn btn-primary" aria-hidden="true">Ajouter</button>
+        </div>
+    </form>
 </div>
+
+
 
--- resources/views/deliverable/show.blade.php
+++ resources/views/deliverable/show.blade.php
@@ -6,53 +6,56 @@ Description: Show the checkList "Livrables"
 @extends('layouts.project')
 
 @section('projectContent')
-<div class="col-xs-12 col-lg-6">
-  <div class="panel panel-default">
-    <!-- Display all project informations like the members, a description and so on -->
-    <div class="panel-heading showPanel" data-toggle="collapse" data-target=".deliverables">
-        <h1>Livrables <span class="glyphicon glyphicon-chevron-down pull-right"></span></h1>
-    </div>
-    <div class="panel-body deliverables collapse" data-projectid="{{$project->id}}">
-        <div class="deliveriesData">
-          <div class="progressionCheckList">
-              <div class="barre" style="background: linear-gradient(90deg, #20DE13 {{$livrables->getCompletedPercent()}}%, #efefef 0%);"></div>
-              <p>{{$livrables->getNbItemsDone()}}/{{$livrables->getNbItems()}}</p>
-          </div>
-          <div>
-              <!-- Display all livrables -->
-              @if($livrables->showToDo())
-                  @foreach($livrables->showToDo() as $checkListItem)
-                      @include('checkList.show', array('checkListItem'=>$checkListItem, 'projectId'=>$project->id, 'fileData'=>$livrables->getLink($checkListItem->link), 'file'=>true ))
-                  @endforeach
-              @endif
-          </div>
-          <div class="completed hidden">
-            <h3>Effectués</h3>
-            @if($livrables->showCompleted())
-                @foreach($livrables->showCompleted() as $checkListItem)
-                    @include('checkList.show', array('checkListItem'=>$checkListItem, 'projectId'=>$project->id, 'fileData'=>$livrables->getLink($checkListItem->link), 'file'=>true ))
-                @endforeach
-            @endif
-          </div>
+    <div class="panel panel-default">
+        <!-- Display all project informations like the members, a description and so on -->
+        <div class="panel-heading">
+            <h1>Livrables</h1>
         </div>
-
-
         @if(Auth::user()->projects()->find($project->id))
-            @include('deliverable.create')
-            <a class="btn btn-primary newDelivery">Ajouter</a>
+            <div class="form-group col-md-12 div-new-deliverable">
+                <div class="col-md-2">
+                    <label>Nouveau livrable :</label>
+                </div>
+                @include('deliverable.create')
+            </div>
         @endif
-        @if($livrables->getNbItemsDone())
-            <a class="btn btn-primary changeView">Voir les éléments effectués</a>
-            <a class="btn btn-primary changeView hidden">Cacher les éléments effectués</a>
-        @endif
-        <button class="btn btn-primary reloadDeliveries pull-right" data-projectid="{{$project->id}}">
-          <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>
-        </button>
+        <div class="panel-body">
+            <div class="deliveriesData">
+                <div class="progressionCheckList">
+                    <div class="barre"
+                         style="background: linear-gradient(90deg, #20DE13 {{$livrables->getCompletedPercent()}}%, #efefef 0%);"></div>
+                    <p>{{$livrables->getNbItemsDone()}}/{{$livrables->getNbItems()}}</p>
+                </div>
+                <div>
+                    <!-- Display all deliverables -->
+                    @if($livrables->showToDo())
+                        @foreach($livrables->showToDo() as $checkListItem)
+                            @include('checkList.show', array('checkListItem'=>$checkListItem, 'projectId'=>$project->id, 'fileData'=>$livrables->getLink($checkListItem->link), 'file'=>true ))
+                        @endforeach
+                    @endif
+                </div>
+                <div class="completed hidden">
+                    <h3>Effectués</h3>
+                    @if($livrables->showCompleted())
+                        @foreach($livrables->showCompleted() as $checkListItem)
+                            @include('checkList.show', array('checkListItem'=>$checkListItem, 'projectId'=>$project->id, 'fileData'=>$livrables->getLink($checkListItem->link), 'file'=>true ))
+                        @endforeach
+                    @endif
+                </div>
+            </div>
+            @if($livrables->getNbItemsDone())
+                <a class="btn btn-primary changeView">Voir les éléments effectués</a>
+                <a class="btn btn-primary changeView hidden">Cacher les éléments effectués</a>
+            @endif
+        </div>
     </div>
-  </div>
-</div>
 @endsection
 
-@push('scripts')
-    <script src="{{ URL::asset('deliverables.js') }}"></script>
+@section('projectCss')
+    <link rel="stylesheet" href="{{ URL::asset('css/deliverable.css') }}"/>
+@endsection
+
+@push('projectScripts')
+<script src="{{ URL::asset('js/deliverables.js') }}"></script>
+<script src="{{ URL::asset('js/checkList.js') }}"></script>
 @endpush
