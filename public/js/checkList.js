457bd8cd874142351e6e3dad8e8c7ece6421b7df
Antonio Giordano <antonio.giordano17@gmail.com>
2018-06-12 16:00:03 +0200

    Remove objective's function

--- public/js/checkList.js
+++ public/js/checkList.js
@@ -5,53 +5,6 @@
 */
 $(document).ready(function () {
   /**
-   * Add a new checkList
-   */
-  $(".newCheckList").click(function(e) {
-      e.preventDefault()
-      var div = $(this).parent('div'), form = div.parent('form'), url = form.attr('action'), title = form.children('div').children('#name').val()  //$('#name').val())
-
-      $.ajax({
-          url: url,
-          type: 'POST',
-          data: {name: title},
-          success: function (data) {
-              var result = $('<div />').append(data).find('.deliveriesData').html();
-              $(".deliveriesData").html(result);
-              var result = $('<div />').append(data).find('.objectivesData').html();
-              $(".objectivesData").html(result);
-              bootbox.hideAll();
-          }
-      });
-  });
-
-    $(document).on("click", 'a.removeObjective', function(event) {
-      var id = this.getAttribute('data-id');
-      var projectid = this.getAttribute('data-projectid');
-
-      bootbox.confirm("Voulez vous vraiment supprimer cet objectif ?", function (result) {
-          if (result) {
-              $.ajax({
-                  type: "DELETE",
-                  url: "/project/" + projectid + "/objective/" + id,
-                  success: function (data) {
-                      bootbox.alert("Objectif supprimé avec succès");
-                      $.ajax({
-                          url: "",
-                          type: 'get',
-                          success: function (data) {
-                              var result = $('<div />').append(data).find('.objectivesData').html();
-                              $(".objectivesData").html(result)
-                          }
-                      });
-                  }
-              });
-          }
-      });
-  })
-
-  /**
-   *
    * switch view button hidde or not the completed items
    */
   $('.changeView').click(function(){
