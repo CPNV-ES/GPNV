1952f2c938862f395fefe39aa5542ced7bd8a01d
Quentin Neves <neves.quentin@gmail.com>
2019-04-02 15:41:20 +0200

    Fix typo

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -66,7 +66,7 @@
             </div>
         </div>
         <div class="form-group">
-            <label class="col-md-4 control-label">Statut</label>
+            <label class="col-md-4 control-label">Status</label>
 
             <div class="col-md-6 statusContainer">
                 <?php
