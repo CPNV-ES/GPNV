0099894fd0dc2247e8d5acdf707fcf5e60341012
Philippe <philippe.baumann@cpnv.ch>
2019-05-15 08:40:17 +0200

    Redesigned Objectives Tab

--- resources/views/objective/checklist.blade.php
+++ resources/views/objective/checklist.blade.php
@@ -1,52 +1,48 @@
 <!--
   Created By: Fabio Marques
-  Description: View to show each item of the checkList
+  Description: Objectives Blocks Objects
+  Modified By : Philippe Baumann
 -->
 
-<div class="well well-sm checklist-item">
-    <div class="media">
-        <div class="media-body">
-            <div class="col-md-12">
-                @if(isset($modalBox))
-                    <a class="btn removeObjective pull-right icon-checklist" data-id="{{$checkListItem->id}}"
-                       data-projectid="{{$project->id}}">
-                        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                    </a>
-                @endif
+<div class="col-md-12 well well-sm checklist-item">
+    @if(isset($modalBox))
+        <a class="btn removeObjective pull-right icon-checklist" data-id="{{$checkListItem->id}}"
+           data-projectid="{{$project->id}}">
+            <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </a>
+    @endif
 
-                @if(isset($modalBox) && $modalBox)
-                    <a class="showObjectif" data-id="{{$checkListItem->id}}">
-                @else
-                    <a/>
-                @endif
+    @if(isset($modalBox) && $modalBox)
+        <a class="showObjectif" data-id="{{$checkListItem->id}}">
+    @else
+        </a>
+    @endif
 
-                <label>{{$checkListItem->title}}</label>
+    <!-- Objective title -->
+    <label class="objective-title">{{$checkListItem->title}}</label>
 
+    <!-- Objective scenarios stats -->
+    @if(isset($scenarios))
+        <label class="pull-right">
+        @if($scenarios['allComplete'])
+            <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+        @else
+                {{$scenarios['nbScenarios']}}/{{$scenarios['nbValidateScenarios']}}/{{$scenarios['nbCompletedScenarios']}}
+        @endif
+        </label>
+    @endif
 
-                @if(isset($scenarios))
-                    <label class="pull-right">
-                    @if($scenarios['allComplete'])
-                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                    @elseif($scenarios['nbScenarios']!=0)
-                        {{$scenarios['nbScenarios']}}/{{$scenarios['nbValidateScenarios']}}
-                        /{{$scenarios['nbCompletedScenarios']}}
-                    @else
-                        ?
-                    @endif
-                    </label>
-                @endif
-                    <a class="btn pull-right icon-checklist updateObjective" data-id="{{$checkListItem->id}}">
-                        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
-                    </a>
-                        <div id="{{$checkListItem->id}}" class="col-md-12 hidden">
-                            @include("objective.form")
-                        </div>
+    <!-- Objective actions -->
+    <a class="btn pull-right icon-checklist updateObjective" data-id="{{$checkListItem->id}}">
+        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+    </a>
 
-                </a>
-                <input type="hidden" id="validate" name="validate" value="true"/>
-            </div>
-        </div>
+    <div id="{{$checkListItem->id}}" class="col-md-12 hidden">
+        @include("objective.form")
     </div>
+
+    </a>
+    <input type="hidden" id="validate" name="validate" value="true"/>
 </div>
 
 @push('projectCss')
