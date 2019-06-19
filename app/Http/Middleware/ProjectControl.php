13945fb3536d102f177f3fb23d0aaebf03d2b725
Quentin Neves <neves.quentin@gmail.com>
2019-06-19 08:57:25 +0200

    Fix ProjectControl middleware

--- app/Http/Middleware/ProjectControl.php
+++ app/Http/Middleware/ProjectControl.php
@@ -18,7 +18,7 @@ class ProjectControl extends Validator
      */
     public function handle($request, Closure $next)
     {
-      if (Auth::user()->projects()->find($request->id) || Auth::user()->role_id==2){
+      if (Auth::user()->projects()->find($request->project) || Auth::user()->role_id==2){
         if(($request->isMethod('post') || $request->isMethod('delete')) && Auth::user()->projects()->find($request->id))
           return $next($request);
         elseif ($request->isMethod('get'))
